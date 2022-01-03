8e5b76115993a637e9bad77b6ef4279a9713d19a
prado <antonio.ti.celso@gmail.com>
2022-01-03 17:11:32 +0000

    Creat: class Person, Phone, PhoneType

---
+++ src/main/java/com/github/antonioelso/personapi/entity/Phone.java
@@ -0,0 +1,19 @@
+package com.github.antonioelso.personapi.entity;
+
+import com.github.antonioelso.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    private Long id;
+    private PhoneType type;
+    private String number;
+
+}
