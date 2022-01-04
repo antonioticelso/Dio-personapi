221098046264f5381fbf69846e7fb94ad07587c4
prado <antonio.ti.celso@gmail.com>
2022-01-04 00:24:23 +0000

    Refactor: Atualizaçao de class: PersonController

--- src/main/java/com/github/antonioelso/personapi/cotroller/PersonController.java
+++ src/main/java/com/github/antonioelso/personapi/cotroller/PersonController.java
@@ -1,15 +1,29 @@
 package com.github.antonioelso.personapi.cotroller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.antonioelso.personapi.dto.response.MessageResponseDTO;
+import com.github.antonioelso.personapi.entity.Person;
+import com.github.antonioelso.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "API Test!";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savePerson.getId())
+                .build();
+    }
+
 }
