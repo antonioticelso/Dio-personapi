dfc6f4a323993c5b0ac3e0c016c4dc449cf3b42f
prado <antonio.ti.celso@gmail.com>
2022-01-05 10:47:31 +0000

    Refactor: Atualizaçao de class: MessageResponseDTO e PersonController; Inclusao de dependencias no pom

--- src/main/java/com/github/antonioelso/personapi/cotroller/PersonController.java
+++ src/main/java/com/github/antonioelso/personapi/cotroller/PersonController.java
@@ -2,28 +2,26 @@ package com.github.antonioelso.personapi.cotroller;
 
 import com.github.antonioelso.personapi.dto.response.MessageResponseDTO;
 import com.github.antonioelso.personapi.entity.Person;
-import com.github.antonioelso.personapi.repository.PersonRepository;
+import com.github.antonioelso.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savePerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savePerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 
 }
