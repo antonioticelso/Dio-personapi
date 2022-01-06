bda77a9c229953c38fb4b7ee81a5a32d18ec4801
prado <antonio.ti.celso@gmail.com>
2022-01-06 19:12:41 +0000

    Creat: Criaçao de class: PersonDTO, PersonMapper, PhoneDTO

---
+++ src/main/java/com/github/antonioelso/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.github.antonioelso.personapi.mapper;
+
+import com.github.antonioelso.personapi.dto.request.PersonDTO;
+import com.github.antonioelso.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+
+}
